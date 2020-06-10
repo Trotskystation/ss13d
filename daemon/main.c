b2491a2cfa369583f032d59c7bd616c2cc3f731c
PaXboi <29384491+PaXboi@users.noreply.github.com>
2020-06-10 23:18:12 +0000

    Push WIP copy for review.

--- daemon/main.c
+++ daemon/main.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Configuration file specified does not exist.\n");
 		return EXIT_FAILURE;
 	}
-	FILE* config = open(argstruct->configpath, O_RDONLY);
+	FILE* config = fopen(argstruct->configpath, "r");
 	if (config == -1) {
 		fprintf(stderr, "Could not open configuration file.\n");
 		return EXIT_FAILURE;
@@ -82,13 +82,12 @@ int parseconfig(FILE* configfd, struct argstruct* returnstruct) {
 		return 1;
 	}
 	memset(lineptr, 0, sizeof(char*) * linebufcnt));
-	unsigned int linepos = 0;
+	unsigned int linei = 0;
 	size_t linesize = 0;
 	
 	while (linesize >= 0) {
 		/* This is a great addition to a libc and I'm happy it's in POSIX. */
-		getline(lineptr[linepos], &linesize, configfd);
-		
+		getline(lineptr[linei], &linesize, configfd);
 		
 		if (linebufcnt == linepos) {
 			lineptr = realloc(lineptr, sizeof(char*) * linebufcnt + INITLINECNT);
@@ -99,8 +98,19 @@ int parseconfig(FILE* configfd, struct argstruct* returnstruct) {
 			memset(lineptr + sizeof(char*) * linebufcnt, 0);
 			linebufcnt = linebufcnt + INITLINECNT;
 		}
+		linei++;
 	}
-	assert(false);
+	
+	for(unsigned int i=0; i>=linei; i++) {
+		if (lineptr[i][0] == '#') {
+			continue;
+		}
+		char* token = strtok(lineptr[i], " ");
+		/* May switch to a enum or something in the future. */
+		if (strcmp(token, "SERVERROOT") {
+			
+		
+	}
 	return 0;
 }
 void usage(void) {
