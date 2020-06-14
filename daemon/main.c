2e13f04f73ba0674b06adca0ca47bb2988169182
PaXboi <29384491+PaXboi@users.noreply.github.com>
2020-06-13 19:44:48 -0500

    Better error handling in daemon, refactoring root dir code.

--- daemon/main.c
+++ daemon/main.c
@@ -12,11 +12,13 @@
  * 
  * You should have received a copy of the GNU Affero General Public 
  * License along with SS13d. If not, see <https://www.gnu.org/licenses/>. */
-
- #define INITLINECNT 64
+ 
+ #define PROGIDENT "SS13d (Space Station 13 daemon)"
+ #define DEFAULTSERVERROOT "ss13_root/"
  #define AROOTPATH "aroot/"
  #define BROOTPATH "broot/"
  #define CFGROOTPATH "cfg/"
+ #define BYONDROOTPATH "byond/"
  
  #include <dirent.h>
  #include <stdio.h>
@@ -27,66 +29,96 @@
  #include <unistd.h>
  
 struct argstruct {
-	char* serverroot;
-	char* aroot;
-	char* broot;
-	char* cfgroot;
-	char* repo;
+	char** byondroot;
+	char** serverroot;
+	char** aroot;
+	char** broot;
+	char** cfgroot;
+	char** repo;
 };
 
 void usage(void);
 DIR* openorinitdir(char* path);
 char* concatpath(char* str1, char* str2);
+void exit_error(char* errstr);
 
 int main(int argc, char *argv[]) {
+	openlog(PROGIDENT, LOG_PID, LOG_DAEMON);
+	
 	struct argstruct* args = malloc(sizeof(args));
 	if (args == NULL) {
-		fprintf(stderr, "argstruct malloc() failed.\n");
+		exit_error("argstruct malloc() failed.");
 		return EXIT_FAILURE;
 	}
 	
 	opterr = 0;
 	
 	int arg;
-	while ((arg = getopt(argc, argv, "+hr:")) != -1) {
+	char* uidarg = 0;
+	char* gidarg = 0;
+	while ((arg = getopt(argc, argv, "+hu:g:r:G:")) != -1) {
 		switch (arg) {
 			case 'h':
 				usage();
 				return EXIT_SUCCESS;
+			case 'u':
+				uidarg = optarg;
+				break;
+			case 'g':
+				gidarg = optarg;
+				break;
 			case 'r':
 				args->serverroot = optarg;
 				break;
+			case 'G':
+				args->repo = optarg;
+				break;
 			default:
-				fprintf(stderr, "Invalid argument.\n");
+				fprintf(stderr, "Invalid argument. \"-u\" for usage information.");
 				return EXIT_FAILURE;
 				break;
 			}
 		}
-	
-	DIR* root = openorinitdir(args->serverroot);
-	if (root == NULL) {
-		fprintf(stderr, "Could not open directory.");
+		
+	if (atoi(gidarg) != 0) {
+		if (setgid(atoi(gidarg)) != 0) {
+			exit_error("Could not change GID to specified!");
+		}
+		if (setuid(atoi(uidarg)) != 0) {
+			exit_error("Could not change UID to specified!");
+		}
 	}
-	char* arootstr = malloc(strlen(args->serverroot) + strlen(AROOTPATH) + 1);
-	char* brootstr = malloc(strlen(args->serverroot) + strlen(BROOTPATH) + 1);
-	char* cfgrootstr = malloc(strlen(args->serverroot) + strlen(CFGROOTPATH) + 1);
-	if (arootstr == NULL || brootstr == NULL || cfgrootstr == NULL) {
-		fprintf(stderr, "malloc() path strings failed.");
+	
+	if (getuid() == 0) {
+		syslog(LOG_WARNING, "Warning: Running as root!");
+	}
+	
+	if (args-serverroot != NULL) {
+		DIR* root = openorinitdir(args->serverroot);
+		if (root == NULL) {
+			exit_error( "Could not open directory.");
+		}
+		char* arootstr = malloc(strlen(args->serverroot) + strlen(AROOTPATH) + 1);
+		char* brootstr = malloc(strlen(args->serverroot) + strlen(BROOTPATH) + 1);
+		char* cfgrootstr = malloc(strlen(args->serverroot) + strlen(CFGROOTPATH) + 1);
+		char* byondrootstr = malloc(strlen(args->serverroot) + strlen(BYONDROOTPATH) + 1);
+		
+		if (arootstr == NULL || brootstr == NULL || cfgrootstr == NULL || byondrootstr == NULL ) {
+			exit_error("malloc() path strings failed.");
+		}
+		arootstr = concatpath(arootstr, AROOTPATH);
+		brootstr = concatpath(brootstr, BROOTPATH);
+		cfgrootstr = concatpath(cfgrootstr, CFGROOTPATH);
+		byondrootstr = concatpath(byondrootstr, CFGROOTPATH);
 	}
 	
 }
 
-DIR* openorinitdir(char* path) {
-	DIR* pathfd = opendir(path);
-	if (pathfd == NULL) {
-		if (mkdir(path, 0711) == -1) {
-			return NULL;
-		}
-		DIR* pathfd = opendir(path);
-		if (pathfd == NULL) {
-			return NULL;
-		}
+DIR* initopendir(char* path) {
+	if (mkdir(path, 0755) == -1) {
+		return NULL;
 	}
+	DIR* pathfd = opendir(path);
 	return pathfd;
 }
 
@@ -103,7 +135,17 @@ char* concatpath(char* basepath, char* concatent) {
 }
 
 void usage(void) {
-	printf("-h: Print usage.\
--r: Specify server root directory. Use absolute paths.\
--g: Specify server git repository.");
+	printf("-h: Print usage.\n\
+-u: Optional, specify UID to drop privs to.\n\
+-g: Optional, specify GID to drop privs to.\n\
+-r: Optional, specify server root directory. Use absolute paths. If not used,\
+you must specify a UID/GID and we will try to use the home directory. \n\
+-G: Specify server git repository. Protocol: git://\n");
+}
+
+void exit_error(char* errstr) {
+	fprintf(stderr, "%s\n", errstr);
+	syslog(LOG_CRIT, "%s", errstr);
+	closelog();
+	exit(EXIT_FAILURE);
 }
